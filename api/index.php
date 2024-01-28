d14274a1bdae41eb9aa679a242204ca047429dba
DavidCh <david.chandra0501@gmail.com>
2024-01-28 21:56:18 +0700

    vercel

--- api/index.php
+++ api/index.php
@@ -3,7 +3,7 @@
 // Load the Laravel application
 require __DIR__ . '/../vendor/autoload.php';
 $app = require_once __DIR__ . '/../bootstrap/app.php';
- 
+
 // Run the application
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 $response = $kernel->handle(
